249cec5dac760156c1289b7db7ac43c51ef9a279
Farhan Ali <farhan94@gmail.com>
2021-10-08 02:16:34 -0500

    added collection page quick buy flow

--- content.js
+++ content.js
@@ -1,9 +1,23 @@
-
-
-
-
-// let insertedButton = "<a href=\"\" class=\"button\">Go to Google</a>"
+/*
+This init function will initially scan for the activity panel and continually scan for it if it can't find it
+*/
 async function init(){
+    if(location.href.includes("opensea.io/activity")) {
+        // activity flow
+        initActivity()
+    } else if (location.href.includes("opensea.io/collection")) {
+        // collection flow
+        initCollection()
+    } else if (location.href.includes("opensea.io/assets")) {
+        // assets flow
+        return
+    } else {
+        return
+    }
+    
+}
+
+async function initActivity() {
     let panelList = document.getElementsByClassName("EventHistory--Panel")
     while(panelList.length == 0) {
         await delay(500)
@@ -29,13 +43,6 @@ async function init(){
     console.log(elements.length)
     for(let i = 0; i < elements.length; i++) {
 
-        // let link = document.createElement("a")
-        // link.id = "purchase"
-        // link.title = "Purchase"
-        // link.href = "google.com"
-        // link.textContent = "Purchase"
-        // elements[i].appendChild(link)
-        
         let linksList = elements[i].getElementsByTagName("a")
         if(linksList.length == 1) {
             // console.log("detected observed")
@@ -45,9 +52,48 @@ async function init(){
             link.title = "Purchase"
             link.href = "javascript:void(0);" //linksList[0].href
             link.textContent = "Purchase"
+            link.className = "purchase"
             elements[i].appendChild(link)
         }
     }
+
+}
+
+async function initCollection(){
+    //start by getting the grid - we want to set the observer to observe this
+    let gridList = document.getElementsByClassName("AssetSearchView--results collection--results")
+    while(gridList.length == 0) {
+        await delay(500)
+        gridList = document.getElementsByClassName("AssetSearchView--results collection--results")
+    }
+    let grid = gridList[0]
+
+    let gridCells = grid.getElementsByClassName("Assetreact__DivContainer-sc-bnjqwy-0 gwTKfX Asset--loaded AssetSearchList--asset")
+    while(gridCells.length == 0) {
+        await delay(500)
+        gridCells = document.getElementsByClassName("Assetreact__DivContainer-sc-bnjqwy-0 gwTKfX Asset--loaded AssetSearchList--asset")
+    }
+    //add purchase link to each item same logic as activity
+    for(let i = 0; i < gridCells.length; i++) {
+        let purchaseLinks = gridCells[i].getElementsByClassName("purchase")
+        if (purchaseLinks.length == 0) {
+                
+            let linksList = gridCells[i].getElementsByTagName("a")
+            // console.log("detected observed")
+            let link = document.createElement("a")
+            link.id = "purchase"
+            link.onclick = function() {autoCheckOut(linksList[0])}
+            link.title = "Purchase"
+            link.href = "javascript:void(0);" //linksList[0].href
+            link.textContent = "Purchase"
+            link.className = "purchase"
+            gridCells[i].appendChild(link)
+        }
+        
+    }
+
+    // TODO: set the observer to observe grid for changes and add links to them
+    observer.observe(grid, {childList : true, subtree : true })
 
 }
 
@@ -65,81 +111,10 @@ The following function will start the auto checkout process
 */
 function autoCheckOut(link) {
     chrome.runtime.sendMessage({redirect: true});
-    // chrome.storage.local.set({"autoCheckOutOn": true}, function(){console.log("auto set")})
     link.click()
-    // window.location.href = link
     
 }
 
-
-function createNewObserver(){
-    return new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutationRecord) {    //runs for each change to target declared below
-            // console.log("detected change")
-            mutationRecord.addedNodes.forEach(function(addedNode) {
-                if(addedNode.nodeType === 1) {  // making sure the Node we are getting is an HTML element
-                    if (addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row EventHistory--polledData" || addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row") {
-                        //only want to take this route if it is an actual nft that is listed. polledData is for new listings, the other class name is for past listings when we scroll down
-                        let itemList = addedNode.getElementsByClassName("Row--cell Row--cellIsSpaced EventHistory--item-col")
-                        //get just the item element for the event row
-                        if(itemList.length != 0){
-                            for(let i = 0; i < itemList.length; i++) {
-                            let linksList = addedNode.getElementsByTagName("a") //get the link element for the item
-                            if(linksList.length == 1) {
-                                // console.log("detected observed")
-                                let link = document.createElement("a")
-                                link.id = "purchase"
-                                link.onclick = function() {autoCheckOut(linksList[0])}
-                                link.title = "Purchase"
-                                link.href = "javascript:void(0);"
-                                link.textContent = "Purchase"
-                                itemList[i].appendChild(link) //created and added a button to kick off purchase script
-                            }
-                        }
-                        
-    
-                        }
-                    }
-                }
-            });
-        })
-    });
-}
-
-async function addButton(){
-    let elements = document.getElementsByClassName("Row--cell Row--cellIsSpaced EventHistory--item-col")
-    while(elements.length < 2) {
-        console.log("wait for ele")
-        await delay(500)
-        elements = document.getElementsByClassName("Row--cell Row--cellIsSpaced EventHistory--item-col")
-    }
-    console.log("got ele")
-    console.log(elements.length)
-    for(let i = 0; i < elements.length; i++) {
-
-        // let link = document.createElement("a")
-        // link.id = "purchase"
-        // link.title = "Purchase"
-        // link.href = "google.com"
-        // link.textContent = "Purchase"
-        // elements[i].appendChild(link)
-        
-        let linksList = elements[i].getElementsByTagName("a")
-        if(linksList.length == 1) {
-            // console.log("detected observed")
-            let link = document.createElement("a")
-            link.id = "purchase"
-            link.onclick = function() {autoCheckOut(linksList[0])}
-            link.title = "Purchase"
-            link.href = "javascript:void(0);" //linksList[0].href
-            link.textContent = "Purchase"
-            elements[i].appendChild(link)
-        }
-    }
-
-
-
-}
 
 async function monitorForPageChange() {
     while(true) {
@@ -148,14 +123,6 @@ async function monitorForPageChange() {
             if(result.pageChanged != null && result.pageChanged == true){
                 console.log(result)
                 chrome.storage.local.set({"pageChanged": false}, function(){console.log("reinitalized page changed")})
-                // let panelList = document.getElementsByClassName("ActivitySearch--history")
-                // while(panelList.length == 0) {
-                //     await delay(100)
-                //     panelList = document.getElementsByClassName("ActivitySearch--history")
-                // }
-                // console.log("found Panel")
-                // let target = panelList[0]
-                // observer.observe(target, {childList : true, subtree : true })
                 chrome.storage.local.get(["autoCheckOutOn"], function(result){
                     console.log(result)
                     if(result.autoCheckOutOn == true){
@@ -175,35 +142,72 @@ async function monitorForPageChange() {
 let observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutationRecord) {    //runs for each change to target declared below
         console.log("detected change")
-        mutationRecord.addedNodes.forEach(function(addedNode) {
-            if(addedNode.nodeType === 1) {  // making sure the Node we are getting is an HTML element
-                if (addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row EventHistory--polledData" || addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row") {
-                    //only want to take this route if it is an actual nft that is listed. polledData is for new listings, the other class name is for past listings when we scroll down
-                    let itemList = addedNode.getElementsByClassName("Row--cell Row--cellIsSpaced EventHistory--item-col")
-                    //get just the item element for the event row
-                    if(itemList.length != 0){
-                        for(let i = 0; i < itemList.length; i++) {
-                        let linksList = itemList[i].getElementsByTagName("a") //get the link element for the item
-                        if(linksList.length == 1) {
-                            // console.log("detected observed")
-                            let link = document.createElement("a")
-                            link.id = "purchase"
-                            link.onclick = function() {autoCheckOut(linksList[0])}
-                            link.title = "Purchase"
-                            link.href = "javascript:void(0);"
-                            link.textContent = "Purchase"
-                            itemList[i].appendChild(link) //created and added a button to kick off purchase script
-                        }
-                    }
-                    
-
-                    }
-                }
-            }
-        });
+        if(location.href.includes("opensea.io/activity")) {
+            // activity flow
+            changeActivity(mutationRecord)
+        } else if (location.href.includes("opensea.io/collection")) {
+            // collection flow
+            changeCollection(mutationRecord)
+        } else {
+            return
+        }
     })
 });
 
+function changeActivity(mutationRecord) {
+    mutationRecord.addedNodes.forEach(function(addedNode) {
+        if(addedNode.nodeType === 1) {  // making sure the Node we are getting is an HTML element
+            if (addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row EventHistory--polledData" || addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row") {
+                //only want to take this route if it is an actual nft that is listed. polledData is for new listings, the other class name is for past listings when we scroll down
+                let itemList = addedNode.getElementsByClassName("Row--cell Row--cellIsSpaced EventHistory--item-col")
+                //get just the item element for the event row
+                if(itemList.length != 0){
+                    for(let i = 0; i < itemList.length; i++) {
+                    let linksList = itemList[i].getElementsByTagName("a") //get the link element for the item
+                    if(linksList.length == 1) {
+                        // console.log("detected observed")
+                        let link = document.createElement("a")
+                        link.id = "purchase"
+                        link.onclick = function() {autoCheckOut(linksList[0])}
+                        link.title = "Purchase"
+                        link.href = "javascript:void(0);"
+                        link.textContent = "Purchase"
+                        itemList[i].appendChild(link) //created and added a button to kick off purchase script
+                    }
+                }
+                
+
+                }
+            }
+        }
+    });
+}
+
+function changeCollection(mutationRecord) {
+    mutationRecord.addedNodes.forEach(function(addedNode) {
+        if(addedNode.nodeType === 1) {  // making sure the Node we are getting is an HTML element
+            let gridCellContentList = addedNode.getElementsByClassName("Assetreact__DivContainer-sc-bnjqwy-0 gwTKfX Asset--loaded AssetSearchList--asset")
+
+            if (gridCellContentList.length != 0) {
+                //gridcell should have something in it
+                let purchaseLinks = gridCellContentList[0].getElementsByClassName("purchase")
+                if (purchaseLinks.length == 0) {
+                        
+                    let linksList = gridCellContentList[0].getElementsByTagName("a")
+                    // console.log("detected observed")
+                    let link = document.createElement("a")
+                    link.id = "purchase"
+                    link.onclick = function() {autoCheckOut(linksList[0])}
+                    link.title = "Purchase"
+                    link.href = "javascript:void(0);" //linksList[0].href
+                    link.textContent = "Purchase"
+                    link.className = "purchase"
+                    gridCellContentList[0].appendChild(link)
+                }
+            }
+        }
+    });
+}
 
 
 init()
@@ -270,116 +274,3 @@ async function check(){
 
 
 }
-
-
-// async function check(){
-//     let retries = 0
-//     while(document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 kmCSYg gMiESj").length == 0) {
-//             await delay(10)
-//             retries++
-//             if(retries == 1000){
-//                 console.log("took too long")
-//                 return
-//             }
-//     }
-//     retries = 0
-//     document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 kmCSYg gMiESj")[0].click()
-//     while(document.getElementById("review-confirmation") == null && document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Modalreact__ModalFooter-sc-xyql9f-4 CheckoutModalreact__StyledFooter-sc-3k02w3-0 dBFmez hLwTLZ iaPZMm").length == 0) {
-//         await delay(10)
-//         retries++
-//         if(retries >= 1000){
-//             console.log("took too long")
-//             return
-//         }
-//         document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 kmCSYg gMiESj")[0].click()
-//     }
-
-//     retries = 0
-//     let conf = document.getElementById("review-confirmation")
-//     let tos = document.getElementById("tos")
-//     console.log(conf)
-//     while (conf===null){
-//         if (tos != null){
-//             //call checkout btn fn
-//             break
-//         }
-//         await delay(50)
-//         retries++
-//         if(retries >= 1000){
-//             console.log("took too long")
-//             return
-//         }
-//         conf = document.getElementById("review-confirmation")
-//     }
-//     if (conf != null) {
-//         conf.click()
-//     } 
-//     //tos
-//     //fn
-//     tos = document.getElementById("tos")
-//     console.log(tos)
-//     while (tos == null){
-//         await delay(50)
-//         retries++
-//         if(retries >= 1000){
-//             console.log("took too long")
-//             return
-//         }
-//         tos = document.getElementById("tos")
-//     }
-//     console.log("-------------")
-//     console.log(tos)
-
-//     tos.click()
-//     let checkoutParent = document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Modalreact__ModalFooter-sc-xyql9f-4 CheckoutModalreact__StyledFooter-sc-3k02w3-0 dBFmez hLwTLZ iaPZMm")
-//     while (checkoutParent.length == 0){
-//         await delay(50)
-//         retries++
-//         if(retries >= 1000){
-//             console.log("took too long")
-//             return
-//         }
-//         checkoutParent = document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Modalreact__ModalFooter-sc-xyql9f-4 CheckoutModalreact__StyledFooter-sc-3k02w3-0 dBFmez hLwTLZ iaPZMm")
-//     }
-//     let checkout = checkoutParent[0].getElementsByTagName("button")[0]
-//     checkout.click()
-
-
-// }
-
-
-
-
-// let observer = new MutationObserver(function(mutations) {
-//     mutations.forEach(function(mutationRecord) {    //runs for each change to target declared below
-//         console.log("detected change")
-//         mutationRecord.addedNodes.forEach(function(addedNode) {
-//             if(addedNode.nodeType === 1) {  // making sure the Node we are getting is an HTML element
-//                 if (addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row EventHistory--polledData" || addedNode.className === "Rowreact__DivContainer-sc-amt98e-0 emCxyQ  EventHistory--row") {
-//                     //only want to take this route if it is an actual nft that is listed. polledData is for new listings, the other class name is for past listings when we scroll down
-//                     let itemList = addedNode.getElementsByClassName("Row--cell Row--cellIsSpaced EventHistory--item-col")
-//                     //get just the item element for the event row
-//                     if(itemList.length != 0){
-//                         let linksList = addedNode.getElementsByTagName("a") //get the link element for the item
-//                         if(linksList.length != 0) {
-//                             console.log("detected observed")
-//                             let link = document.createElement("a")
-//                             link.id = "purchase"
-//                             link.onclick = function() {autoCheckOut(linksList[0].href)}
-//                             link.title = "Purchase"
-//                             link.href = "javascript:void(0);"
-//                             link.textContent = "Purchase"
-//                             itemList[0].appendChild(link) //created and added a button to kick off purchase script
-//                         }
-//                     }
-//                 }
-//             }
-//         });
-//     })
-// });
-
-// //setting the target to the event history scroll box
-// //we only care about additions to this element
-// let target = document.getElementsByClassName("EventHistory--Panel")[0].getElementsByClassName("Scrollbox--content")[0];
-
-// observer.observe(target, {childList : true, subtree : true })
